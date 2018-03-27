c3f395e35934bf3d17882e4cdd4973eb91a8de12
Arthur Manning <atmanning@emmaus.edu>
2018-03-26 21:54:22 -0500

    initial commit

---
+++ c12Examples/src/c12Examples/c12Main.java
@@ -0,0 +1,20 @@
+/**
+ *   file: c12Main.java
+ */
+package c12Examples;
+
+/**
+ * @author atmanning - atmanning@dbq.edu
+ *
+ */
+public class c12Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
